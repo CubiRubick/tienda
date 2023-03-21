f227b6b19d025b672e72ad3499d618de50244990
CubiRubick <Jose_Guzman_Villegas@outlook.com>
2023-03-21 17:07:39 -0600

    valid form register

--- basar/views.py
+++ basar/views.py
@@ -7,8 +7,12 @@ class homeDash(TemplateView):
     template_name = 'aut/login.html'
 
 def register_request(request):
+    response = ""
     if request.method == 'POST':
         form = registerform(data=request.POST)
         if form.is_valid():
             form.save()
-    return render(request, 'aut/register.html', {'formulario': form})
+            response = "Registrado Correctamente"
+        else:
+            response = " Usuario o Correo ya han sido utilizados"
+    return render(request, 'aut/register.html',  {'response': response})
--- ventas/settings.py
+++ ventas/settings.py
@@ -45,6 +45,8 @@ INSTALLED_APPS = [
     'django.contrib.sites',
     'allauth',
     'allauth.account',
+    'allauth.socialaccount',
+    'allauth.socialaccount.providers.openid',
 ]
 
 MIDDLEWARE = [
